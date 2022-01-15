430172def40cf3645936f578f38fc3b8f93bc537
aprilnicole013 <aprilnicole013@gmail.com>
2022-01-14 22:25:56 -0500

    URLs for fetch functions added

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //TODO:
 //random meal
 function getRandomMeal(){
-    fetch()
-}
-//meal based on search
-function getMealsBySearch(term){
-
+    fetch('www.themealdb.com/api/json/v1/1/random.php')
 }
 //meal by ID
 function getMealById(id){
-
+    fetch('www.themealdb.com/api/json/v1/1/lookup.php?i='+id)
+}
+//meal based on search
+function getMealsBySearch(term){
+    fetch('www.themealdb.com/api/json/v1/1/search.php?s='+term)
 }
